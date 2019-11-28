584698466019050f30263df76e5d1e3236c94bfd
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2019-11-28 23:38:46 +0300

    Проверка существования треугольника [#6]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class Triangle {
+    public static boolean exist (double ab, double ac, double bc){
+
+        if (ab+ac>bc&&ac+bc>ab&&ab+bc>ac){
+            return true;
+        }
+        return false;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class TriangleTest {
+    @Test
+    public void whenExist(){
+        // при указании другого ожидаемого результата кидает ошибку
+        boolean result = Triangle.exist(2, 2, 2);
+        assertThat(result, is(true));
+    }
+    @Test
+    public void whenNotExist(){
+        boolean result = Triangle.exist(5, 5, 12);
+        assertThat(result, is(false));
+    }
+}
