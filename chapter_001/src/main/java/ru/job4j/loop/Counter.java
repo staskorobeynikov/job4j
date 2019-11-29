1f1102d2b727ba62940a792b97e800440c71033d
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2019-11-29 12:02:57 +0300

    Внесение исправлений. Подсчет суммы[#11]

--- chapter_001/src/main/java/ru/job4j/loop/Counter.java
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -4,10 +4,9 @@ public class Counter {
     public int add(int start, int finish){
         int sum = 0;
         for(start=0; start<=finish; start++){
-            if (start%2!=0){
-                continue;
+            if (start%2==0){
+                sum = sum + start;
             }
-            sum = sum + start;
         }
         return sum;
     }
