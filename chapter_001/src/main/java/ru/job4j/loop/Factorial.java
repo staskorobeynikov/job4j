b4b530946440ee9ab45ac5c152af4b29d1c349c4
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2019-11-29 13:23:42 +0300

    Расчет факториала[#13]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class Factorial {
+    public int calc(int n){
+        int result = 1;
+        if (n==0){
+            result = 1;
+        }
+        for(int i=1; i<=n; i++){
+            result = result * i;
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FactorialTest {
+    @Test
+    public void whenCalculateFactorialForFiveThenOneHundreedTwenty(){
+        Factorial check = new Factorial();
+        int result = check.calc(5);
+        assertThat(result, is(120));
+    }
+    @Test
+    public void whenCalculateFactorialForZeroThenOne(){
+        Factorial check = new Factorial();
+        int result = check.calc(0);
+        assertThat(result, is(1));
+    }
+    @Test
+    public void whenCalculateFactorialForOneThenOne(){
+        Factorial check = new Factorial();
+        int result = check.calc(1);
+        assertThat(result, is(1));
+    }
+}
