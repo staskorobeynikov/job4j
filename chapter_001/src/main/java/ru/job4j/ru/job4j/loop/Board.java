b54a89fc95ccb16db6487c5c86e8f73c3a0b37cb
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2019-11-29 13:50:18 +0300

    Шахматная доска[#14]

---
+++ chapter_001/src/main/java/ru/job4j/ru/job4j/loop/Board.java
@@ -0,0 +1,32 @@
+package ru.job4j.ru.job4j.loop;
+
+public class Board {
+    public static void paint(int width, int height) {
+        for (int row = 0; row < height; row++) {
+            for (int cell = 0; cell < width; cell++) {
+                // условие проверки, что писать пробел или X
+                // Выше в задании мы определили закономерность, когда нужно проставлять X
+                if ((row+cell)%2==0) {
+                    System.out.print("X");
+                } else {
+                    // для наглядности пропуск заменил на "O"
+                    System.out.print("O");
+                }
+            }
+            // добавляем перевод на новую строку.
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        paint(3, 3);
+        System.out.println();
+        paint(4, 4);
+        System.out.println();
+        paint(5,5);
+        System.out.println();
+        paint(6,4);
+        System.out.println();
+        paint(4,6);
+    }
+}
