f0c3f03f15a501cfd53050a4f83d57bf10cef586
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2019-11-29 22:18:50 +0300

    Заполнение массивастепенями чисел[#22]

---
+++ chapter_001/src/test/java/ru/job4j/array/SquareTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat ;
+
+public class SquareTest {
+    @Test
+    public void whenBound3Then149() {
+        int bound = 3;
+        Square square = new Square();
+        int[] rst = square.calculate(bound);
+        int[] expect = new int[] {1, 4, 9};
+        assertThat(rst, is(expect));
+    }
+    @Test
+    public void whenBound5Then1491625() {
+        int bound = 5;
+        Square square = new Square();
+        int[] rst = square.calculate(bound);
+        int[] expect = new int[] {1, 4, 9, 16, 25};
+        assertThat(rst, is(expect));
+    }
+    @Test
+    public void whenBound8Then1491625364964() {
+        int bound = 8;
+        Square square = new Square();
+        int[] rst = square.calculate(bound);
+        int[] expect = new int[] {1, 4, 9, 16, 25, 36, 49, 64};
+        assertThat(rst, is(expect));
+    }
+}
