f7e70d728541ea9dafb3e639dc96506cfc57c663
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2019-11-30 23:08:48 +0300

    Выигрышные комбинации в сокобан[#44]

---
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,71 @@
+package ru.job4j.array;
+
+public class MatrixCheck {
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        int countcell=0;
+        int countrow=0;
+        for (int row = 0; row < board.length; row++) {
+            for (int cell = 0; cell < board.length; cell++) {
+                char sign = board[row][cell];
+                System.out.print(sign);
+                //for () { проверить последовательность.
+                for (row=0; row < board.length; row++){
+                    if(board[row][row]!=' '){
+                        for (cell=0; cell<board.length; cell++){
+                            if(board[cell][row]!=' '){
+                                countcell++;
+                            }
+                            if(countcell==board.length){
+                                result = true;
+                                break;
+                            }
+                            if(board[row][cell]!=' '){
+                                countrow++;
+                            }
+                            if(countrow==board.length){
+                                result = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+                break;
+            }
+            System.out.println();
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        char[][] hasWinVertical = {
+                {'_', '_', 'X', '_', '_'},
+                {'_', '_', 'X', '_', '_'},
+                {'_', '_', 'X', '_', '_'},
+                {'_', '_', 'X', '_', '_'},
+                {'_', '_', 'X', '_', '_'},
+        };
+        boolean win = isWin(hasWinVertical);
+        System.out.println("A board has a winner : " + win);
+        System.out.println();
+        char[][] hasWinHor = {
+                {'_', '_', '_', '_', '_'},
+                {'X', 'X', 'X', 'X', 'X'},
+                {'_', '_', '_', '_', '_'},
+                {'_', '_', '_', '_', '_'},
+                {'_', '_', '_', '_', '_'},
+        };
+        boolean winHor = isWin(hasWinHor);
+        System.out.println("A board has a winner : " + winHor);
+        System.out.println();
+        char[][] notWin = {
+                {'_', '_', 'X', '_', '_'},
+                {'_', '_', 'X', '_', '_'},
+                {'_', 'X', '_', '_', '_'},
+                {'_', '_', 'X', '_', '_'},
+                {'_', '_', 'X', '_', '_'},
+        };
+        boolean lose = isWin(notWin);
+        System.out.println("A board has a winner : " + lose);
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.array;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MatrixCheckTest {
+    @Test
+    public void whenDataMonoByTrueThenTrue() {
+        char[][] input = {
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+        };
+        boolean result = MatrixCheck.isWin(input);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenDataNotMonoByTrueThenFalse() {
+        char[][] input = {
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', 'X', ' ', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+        };
+        boolean result = MatrixCheck.isWin(input);
+        assertThat(result, is(false));
+    }
+    @Test
+    public void whenDataNotMonoByTrueThenFalse1() {
+        char[][] input = {
+                {' ', ' ', ' ', ' ', ' '},
+                {'X', 'X', 'X', 'X', 'X'},
+                {' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' '},
+        };
+        boolean result = MatrixCheck.isWin(input);
+        assertThat(result, is(true));
+    }
+}
