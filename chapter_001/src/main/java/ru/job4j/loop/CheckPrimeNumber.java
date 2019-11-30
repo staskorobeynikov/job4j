2132049e74816e031c381cb2d66469705f7b55ce
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2019-11-30 15:51:04 +0300

    Исправление. Поиск простого числа[#42]

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -6,7 +6,7 @@ public class CheckPrimeNumber {
         if(num < 2){
             prime = false;
         }
-        for(int i=2; i<=num; i++){
+        for(int i=2; i<num; i++){
             if (num%i==0){
                 prime=false;
                 break;
