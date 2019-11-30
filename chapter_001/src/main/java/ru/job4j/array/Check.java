a1d42371c20213899bccc974e5c4c9f35ecc8744
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2019-11-30 13:37:35 +0300

    Исправление.Массив заполнен true или false[#36]

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -2,11 +2,9 @@ package ru.job4j.array;
 
 public class Check {
     public boolean mono(boolean[] data) {
-        boolean result = false;
+        boolean result = true;
         for (int i=0; i<data.length; i++) {
-            if(data[i]==data[0]){
-                result=true;
-            } else {
+            if(data[i]!=data[0]){
                 result=false;
                 break;
             }
