99f50a625162065809a623e98d852c4e79eae220
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2019-11-30 13:56:05 +0300

    Исправление.Поиск минимального числа [#39]

--- chapter_001/src/main/java/ru/job4j/array/Min.java
+++ chapter_001/src/main/java/ru/job4j/array/Min.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 public class Min {
     public static int findMin(int[] array) {
         int min = array[0];
-        for (int index = 0; index < array.length; index++) {
+        for (int index = 1; index < array.length; index++) {
            if(min > array[index]){
                min = array[index];
            }
