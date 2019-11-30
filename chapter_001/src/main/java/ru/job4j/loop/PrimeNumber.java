a460aae510e2a4d88c229b5205e8134716f84d21
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2019-11-30 11:06:27 +0300

    Исправление. Количество простых чисел[#29]

--- chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -4,14 +4,9 @@ public class PrimeNumber {
     public int calc(int finish){
         int count = 0;
         for(int i=2; i<=finish; i++){
-            for(int j=2; j<=i; j++){
-                if(i%j==0&&i!=j) {
-                    break;
-                } else if(i%j==0&&i==j){
-                    count=count+1;
-                }else{
-                    continue;
-                }
+            CheckPrimeNumber primeNumber = new CheckPrimeNumber();
+            if (primeNumber.check(i)==true){
+                count++;
             }
         }
         return count;
