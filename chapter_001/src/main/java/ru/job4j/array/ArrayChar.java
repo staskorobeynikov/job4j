051a4320983a862e0330250bed90332d72302083
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2019-11-30 13:45:40 +0300

    Исправление.Слово начинается с [#37]

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -2,11 +2,9 @@ package ru.job4j.array;
 
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
-        boolean result = false;
+        boolean result = true;
         for (int i=0; i<pref.length; i++){
-            if (pref[i]==word[i]){
-                result = true;
-            } else {
+            if (pref[i]!=word[i]){
                 result = false;
                 break;
             }
