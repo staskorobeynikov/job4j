b645c8768b8eb2a81c573c288c0f44062c76719b
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2019-11-30 13:52:28 +0300

    Исправление.Слово заканчивается на [#38]

--- chapter_001/src/main/java/ru/job4j/array/EndsWith.java
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -2,11 +2,9 @@ package ru.job4j.array;
 
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
-        boolean result = false;
+        boolean result = true;
         for (int i = 0; i<post.length; i++){
-            if (post[i]==word[word.length-post.length+i]){
-                result = true;
-            } else {
+            if (post[i]!=word[word.length-post.length+i]){
                 result = false;
                 break;
             }
