20c8db53c693927865faec34c672d00f183fb2e6
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2019-12-02 20:32:07 +0300

    1.3. Вызов метода с аргументами.[#203173]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Jukebox.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+public class Jukebox {
+    public void music(int position) {
+        if (position == 1) {
+            System.out.println("Пусть бегут неуклюже пешеходы по лужам\n" +
+                    "А вода по асфальту рекой\n" +
+                    "И не ясно прохожим в этот день непогожий\n" +
+                    "Почему я веселый такой");
+        } else if (position == 2) {
+            System.out.println("Спят усталые игрушки, книжки спят.\n" +
+                    "Одеяла и подушки ждут ребят.\n" +
+                    "Даже сказка спать ложится,\n" +
+                    "Чтобы ночью нам присниться.\n" +
+                    "Ты ей пожелай:\n" +
+                    "Баю-бай.");
+        } else {
+            System.out.println("Песня не найдена");
+        }
+    }
+    public static void main(String[] args){
+        Jukebox song = new Jukebox();
+        song.music(1);
+        System.out.println();
+        song.music(10);
+        System.out.println();
+        song.music(2);
+        System.out.println();
+        song.music(5);
+    }
+}
--- chapter_002/src/main/java/ru/job4j/oop/Student.java
+++ chapter_002/src/main/java/ru/job4j/oop/Student.java
@@ -1,17 +1,18 @@
 package ru.job4j.oop;
 
 public class Student {
-    public void music() {
-        System.out.println("Tra tra tra");
+    public void music(String lyrics) {
+        System.out.println("I can sign a song : " + lyrics);
     }
     public void song() {
         System.out.println("I believe I can fly");
     }
     public static void main(String[] args) {
         Student petya = new Student();
-        petya.music();
-        petya.music();
-        petya.music();
+        String song = "I believe I can fly";
+        petya.music(song);
+        petya.music(song);
+        petya.music(song);
         petya.song();
         petya.song();
         petya.song();
