86aa91966b8736175e3ecf3ab1b9d46c01dba398
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2019-12-02 23:25:26 +0300

    1.7. Состояние объекта.[#203176]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,19 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+    public Battery(int capacity){
+        this.load = capacity;
+    }
+    public void exchange(Battery another){
+        this.load = this.load + another.load;
+        another.load = 0;
+    }
+    public static void main(String[] args){
+        Battery first = new Battery(30);
+        Battery second = new Battery(20);
+        System.out.println("first : " + first.load + ". second : " + second.load);
+        second.exchange(first);
+        System.out.println("first : " + first.load + ". second : " + second.load);
+    }
+}
