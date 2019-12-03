16cfcb422b64c77f46fe74a3cf90f22fa1a81ce6
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2019-12-03 16:52:51 +0300

    4. Расстояние между точками в трехмерном пространстве.[#203167]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -7,17 +7,33 @@ public class Point {
     //public static double distance(int x1, int y1, int x2, int y2) {
     private int x;
     private int y;
+    private int z;
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
     }
+    public Point(int first, int second, int third) {
+        this.x = first;
+        this.y = second;
+        this.z = third;
+    }
     public double distance(Point that) {
         return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+    public double distance3d(Point that) {
+        double result = sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2)
+                +
+                pow(this.z - that.z, 2));
+        return result;
     }
     public static void main(String[] args) {
         Point a = new Point(0, 0);
         Point b = new Point(0, 2);
         double dist = a.distance(b);
+        Point a3d = new Point(0, 0, 0);
+        Point b3d = new Point(0, 3, 4);
+        double dist3d = a3d.distance3d(b3d);
         System.out.println(dist);
+        System.out.println(dist3d);
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -12,4 +12,12 @@ public class PointTest {
         double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
+    @Test
+    public void distance3d() {
+        double expected = 10;
+        Point a = new Point(0, 0, 6);
+        Point b = new Point(0, 8, 0);
+        double out = a.distance3d(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
 }
