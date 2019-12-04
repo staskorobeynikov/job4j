48d8f8e14a06ff5f82845984d9da16d1ed14a324
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2019-12-04 19:28:07 +0300

    4. Переопределение[#203184]

--- chapter_002/src/main/java/ru/job4j/inheritance/JSONReport.java
+++ chapter_002/src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -1,6 +1,8 @@
 package ru.job4j.inheritance;
 
-public class JSONReport {
+public class JSONReport extends TextReport {
+
+    @Override
     public String generate(String name, String body) {
         return "{" + "\n" + "\n" + "name : "
                 + name + "," + "\n"
