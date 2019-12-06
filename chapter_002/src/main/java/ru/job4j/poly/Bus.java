d43f9fc0297edddb2db29b1be6ad920709231bef
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2019-12-06 16:22:52 +0300

    3. Интерфейс Input[#203147]

---
+++ chapter_002/src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,31 @@
+package ru.job4j.poly;
+
+
+public class Bus implements Transport {
+    @Override
+    public void drive() {
+        int amountFuel = 10;
+        if (amountFuel > 0) {
+                System.out.println("Автобус может ехать");
+        }
+    }
+
+    @Override
+    public void amountPassengers(int amount) {
+        System.out.println("В автобусе есть: " + amount + " пассажиров");
+    }
+
+    @Override
+    public int fillCar(int amountFuel) {
+        int cost = amountFuel * 44;
+        return cost;
+    }
+    public static void main(String[] args) {
+        Bus bus = new Bus();
+        bus.drive();
+        bus.amountPassengers(10);
+        int result = bus.fillCar(50);
+        System.out.println("Для заправки автобуса необходимо " + result + " рублей");
+    }
+}
+
---
+++ chapter_002/src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,7 @@
+package ru.job4j.poly;
+
+public interface Transport {
+    void drive();
+    void amountPassengers(int amount);
+    int fillCar(int amountFuel);
+}
