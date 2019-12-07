0e178cbf171598d1ba3367d2a7b1aab9309f2d1d
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2019-12-07 22:06:21 +0300

    9.2. Тесты вывода на консоль в StartUI[#203139]

---
+++ chapter_002/src/test/java/ru/job4j/tracker/RefactStartUITest.java
@@ -0,0 +1,58 @@
+package ru.job4j.tracker;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.StringJoiner;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class RefactStartUITest {
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    @Before
+    public void loadOutput() {
+        System.setOut(new PrintStream(this.out));
+    }
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+    }
+    @Test
+    public void whenPrtMenu() {
+        StubInput input = new StubInput(
+                new String[] {"0"}
+        );
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] { action });
+        String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("Menu.")
+                .add("0. Stub action")
+                .toString();
+        assertThat(new String(out.toByteArray()), is(expect));
+    }
+    @Test
+    public void whenCheckOutput() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("fix bug");
+        tracker.add(item);
+        ShowAllAction act = new ShowAllAction();
+        act.execute(new StubInput(new String[] {}), tracker);
+        String expect = new String("ID: " + item.getId() + "; " + "Name: "
+                + item.getName() + "\r\n");
+        assertThat(new String(out.toByteArray()), is(expect));
+    }
+    @Test
+    public void whenCheckFindName() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        FindByNameAction actual = new FindByNameAction();
+        actual.execute(new StubInput(new String[] {"test1"}), tracker);
+        String expect = new String("ID: " + item.getId() + "; " + "Name: "
+                + item.getName() + "\r\n");
+        assertThat(new String(out.toByteArray()), is(expect));
+    }
+}
