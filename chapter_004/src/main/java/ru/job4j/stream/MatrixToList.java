1cbf97a10ade478b384615beef6001db2200218f
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2019-12-14 00:31:56 +0300

    5. Преобразование матрицы чисел в список чисел.[#202997]

---
+++ chapter_004/src/main/java/ru/job4j/stream/MatrixToList.java
@@ -0,0 +1,19 @@
+package ru.job4j.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MatrixToList {
+    /**
+     * Метод позволяет преобразовать матрицу в список чисел
+     * @param array матрица, которую необходимо преобразовать
+     * @return List<Integer> возвращает преобразованную матрицу в виде списка
+     */
+    public List<Integer> convert(Integer[][] array) {
+        return Arrays.stream(array)
+                .flatMap(mt -> Stream.of(mt))
+                .collect(Collectors.toList());
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/stream/MatrixToListTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.List;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class MatrixToListTest {
+    @Test
+    public void whenMatrixToList() {
+        MatrixToList test = new MatrixToList();
+        List<Integer> actual = test.convert(new Integer[][] {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        });
+        List<Integer> expected = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        assertThat(actual, is(expected));
+    }
+    @Test
+    public void whenMatrixNonQuadraticToList() {
+        MatrixToList test = new MatrixToList();
+        List<Integer> actual = test.convert(new Integer[][] {
+                {1, 2, 3, 4, 5},
+                {6, 7, 8},
+                {9}
+        });
+        List<Integer> expected = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        assertThat(actual, is(expected));
+    }
+}
