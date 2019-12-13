5075ef12a246c4986338141305fc003c465d3f38
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2019-12-13 23:07:15 +0300

    3. Уникальность элементов и сортировка.[#202995]

--- chapter_004/src/main/java/ru/job4j/stream/Profiles.java
+++ chapter_004/src/main/java/ru/job4j/stream/Profiles.java
@@ -1,10 +1,15 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(a -> a.getAddress()).collect(Collectors.toList());
+        return profiles.stream()
+                .map(a -> a.getAddress())
+                .distinct()
+                .sorted(Comparator.comparing(Address::getCity))
+                .collect(Collectors.toList());
     }
 }
--- chapter_004/src/test/java/ru/job4j/stream/ProfilesTest.java
+++ chapter_004/src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -15,10 +15,34 @@ public class ProfilesTest {
                 new Profile(new Address("Gomel", "Narodnaya", 30, 60))
         ));
         List<Address> expected = List.of(
-                new Address("Minsk", "Ozernaya", 10, 30),
                 new Address("Brest", "Lesnaya", 20, 50),
-                new Address("Gomel", "Narodnaya", 30, 60)
+                new Address("Gomel", "Narodnaya", 30, 60),
+                new Address("Minsk", "Ozernaya", 10, 30)
         );
         assertThat(actual, is(expected));
      }
+    @Test
+    public void whenUseFunctionDistinct() {
+        Profiles profilesTest = new Profiles();
+        List<Address> actual = profilesTest.collect(List.of(
+                new Profile(new Address("Minsk", "Ozernaya", 10, 30)),
+                new Profile(new Address("Brest", "Lesnaya", 20, 50)),
+                new Profile(new Address("Vitebsk", "Zapadnaya", 56, 243)),
+                new Profile(new Address("Gomel", "Narodnaya", 30, 60)),
+                new Profile(new Address("Minsk", "Ozernaya", 10, 30)),
+                new Profile(new Address("Mogilev", "Poplavskaya", 20, 35)),
+                new Profile(new Address("Brest", "Lesnaya", 20, 50)),
+                new Profile(new Address("Grodno", "Krasnaya", 18, 97)),
+                new Profile(new Address("Vitebsk", "Zapadnaya", 56, 243))
+        ));
+        List<Address> expected = List.of(
+                new Address("Brest", "Lesnaya", 20, 50),
+                new Address("Gomel", "Narodnaya", 30, 60),
+                new Address("Grodno", "Krasnaya", 18, 97),
+                new Address("Minsk", "Ozernaya", 10, 30),
+                new Address("Mogilev", "Poplavskaya", 20, 35),
+                new Address("Vitebsk", "Zapadnaya", 56, 243)
+        );
+        assertThat(actual, is(expected));
+    }
 }
