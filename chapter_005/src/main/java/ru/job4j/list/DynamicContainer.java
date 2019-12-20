abb6d013085d2be8f9ebae4f3f221eed2b4612f8
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2019-12-20 13:19:31 +0300

    5.3.1. Создать динамический список на базе массива.[#214028]

---
+++ chapter_005/src/main/java/ru/job4j/list/DynamicContainer.java
@@ -0,0 +1,63 @@
+package ru.job4j.list;
+
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class DynamicContainer<E> implements Iterable<E> {
+    private Object[] container;
+    private int size = 0;
+    private int modCount = 0;
+
+    public DynamicContainer(int initialCapacity) {
+        container = new Object[initialCapacity];
+    }
+
+    public void add(E value) {
+        modCount++;
+        ensureCapacity();
+        container[size++] = value;
+    }
+
+    private void ensureCapacity() {
+        if (size >= container.length) {
+            container = Arrays.copyOf(container, container.length * 2);
+        }
+    }
+
+    public E get(int index) {
+        return (E) container[index];
+    }
+
+    public int getLength() {
+        return container.length;
+    }
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private int index = 0;
+            private int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                return index < size ? true : false;
+            }
+
+            @Override
+            public E next() {
+                checkModCount();
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return (E) container[index++];
+            }
+
+            private void checkModCount() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+            }
+        };
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/list/DynamicContainerTest.java
@@ -0,0 +1,53 @@
+package ru.job4j.list;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class DynamicContainerTest {
+    private DynamicContainer<Integer> container;
+    @Before
+    public void init() {
+        container = new DynamicContainer<>(3);
+        container.add(5);
+        container.add(10);
+        container.add(15);
+    }
+
+    @Test
+    public void whenDataToContainerThenIs() {
+        assertThat(container.get(0), is(5));
+    }
+
+    @Test
+    public void whenContainerIsFull() {
+        assertThat(container.getLength(), is(3));
+        container.add(45);
+        assertThat(container.getLength(), is(6));
+    }
+
+    @Test
+    public void whenTestIterator() {
+        Iterator it = container.iterator();
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(5));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(10));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(15));
+        assertThat(it.hasNext(), is(false));
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenContainerException() {
+        Iterator it = container.iterator();
+        it.next();
+        container.add(45);
+        it.next();
+    }
+}
