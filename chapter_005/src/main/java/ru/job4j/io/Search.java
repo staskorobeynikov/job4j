e94c9f013e30af6e2bc418cb63212953117d4d61
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2019-12-27 18:13:36 +0300

    3. Сканирование файловой системы.[#214062]

---
+++ chapter_005/src/main/java/ru/job4j/io/Search.java
@@ -0,0 +1,41 @@
+package ru.job4j.io;
+
+import java.io.File;
+import java.util.*;
+
+/**
+ * Класс позволяет получить список файлов в каталоге
+ *
+ * @author STAS KOROBEYNIKOV
+ * @since 27.12.2019
+ */
+public class Search {
+    private List<File> list = new ArrayList<>();
+    private Queue<File> data = new LinkedList<>();
+
+    /**
+     * Метод позволяет получить содержимое папок
+     * @param parent Путь до каталога, с которого нужно осуществить поиск
+     * @param exts Расширение файлов, которые необходимо получить
+     * @return Список содержимого папки в виде массива
+     */
+    public List<File> files(String parent, List<String> exts) {
+        File fileParent = new File(parent);
+        data.offer(fileParent);
+        while (!data.isEmpty()) {
+            File e = data.poll();
+            for (File file : Objects.requireNonNull(e.listFiles())) {
+                if (file.isDirectory()) {
+                    data.offer(file);
+                } else {
+                    String validate = file.getName().split("\\.")[1];
+                    if (exts.contains(validate)) {
+                        System.out.println(file.getName());
+                        list.add(file);
+                    }
+                }
+            }
+        }
+        return list;
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/io/SearchTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SearchTest {
+
+    @Test
+    public void whenFindSixElementsOnComputer() {
+        Search search = new Search();
+        List list = List.of("txt");
+        String str = "c:/projects/job4j/chapter_006/src/main/java/tmpdir";
+        List<File> result = search.files(str, list);
+        assertThat(result.size(), is(6));
+        List<String> findFiles = List.of("1.txt", "2.txt", "3.txt", "4.txt", "5.txt", "6.txt");
+        for (int i = 0; i < result.size(); i++) {
+            assertEquals(findFiles.get(i), result.get(i).getName());
+        }
+    }
+}
