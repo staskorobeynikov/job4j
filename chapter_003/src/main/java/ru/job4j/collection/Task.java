91912834f4f286b01d06b15f72d1a0863e865713
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2020-01-23 23:00:10 +0300

    Правки. Коллекции. Раздел Set. [#218760]

--- chapter_003/src/main/java/ru/job4j/collection/Task.java
+++ chapter_003/src/main/java/ru/job4j/collection/Task.java
@@ -1,5 +1,7 @@
 package ru.job4j.collection;
 
+import java.util.Objects;
+
 public class Task {
     private String number;
     private String description;
@@ -15,5 +17,22 @@ public class Task {
 
     public String getDescription() {
         return description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Task task = (Task) o;
+        return Objects.equals(number, task.number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
