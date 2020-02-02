2e03192006c4f5cef84e8a3d9b275a901e3c4675
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2020-02-02 10:35:30 +0300

    1. класс Count и метод int increment()[#227926]

---
+++ chapter_010/src/main/java/ru/job4j/monitor/Count.java
@@ -0,0 +1,18 @@
+package ru.job4j.monitor;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
+public class Count {
+    @GuardedBy("this")
+    private int value;
+
+    public synchronized void increment() {
+        this.value++;
+    }
+
+    public synchronized int getValue() {
+        return this.value;
+    }
+}
