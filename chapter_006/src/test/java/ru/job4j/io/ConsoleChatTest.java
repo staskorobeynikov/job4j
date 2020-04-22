c0cbe903ab0758a11a9a0a73994bfbfe51370e9f
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2020-04-22 20:48:31 +0300

    Change test IO[#203102]

--- chapter_006/src/test/java/ru/job4j/io/ConsoleChatTest.java
+++ chapter_006/src/test/java/ru/job4j/io/ConsoleChatTest.java
@@ -1,6 +1,5 @@
 package ru.job4j.io;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
@@ -12,28 +11,26 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class ConsoleChatTest {
+
     private PrintStream stdOut = System.out;
+
     private InputStream stdIn = System.in;
+
     private byte[] bytes = ("привет" + System.lineSeparator() + "закончить").getBytes();
+
     private ByteArrayInputStream in = new ByteArrayInputStream(bytes);
+
     private ByteArrayOutputStream out = new ByteArrayOutputStream();
-    @Ignore
+
     @Test
     public void whenWorkConsoleChat() {
         System.setIn(in);
         System.setOut(new PrintStream(out));
-        new ConsoleChat("c:/projects/job4j/chapter_006/data/conversation.txt",
-                "c:/projects/job4j/chapter_006/data/botphrase.txt").init();
+        new ConsoleChat("./data/conversation.txt",
+                "./data/botphrase.txt").init();
         assertThat(new String(out.toByteArray()), is("Bot: Hello!"
                 + System.lineSeparator() + System.lineSeparator()));
         System.setIn(stdIn);
         System.setOut(stdOut);
-    }
-    @Ignore
-    @Test
-    public void whenGetAnswerBot() {
-        ConsoleChat cc = new ConsoleChat("chapter_006/data/conversation.txt",
-                "c:/projects/job4j/chapter_006/data/botphrase.txt");
-        assertThat(cc.getAnswerBot(), is("Hello!"));
     }
 }
