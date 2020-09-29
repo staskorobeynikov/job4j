1665c8a8e2d9c24d5804d63374df716aad6365f6
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2020-09-29 19:58:37 +0300

    4. final [#334073]

---
+++ chapter_002/src/main/java/ru/job4j/ooa/Airbus.java
@@ -0,0 +1,35 @@
+package ru.job4j.ooa;
+
+public class Airbus extends Aircraft {
+    private static final int COUNT_ENGINE = 2;
+
+    private String name;
+
+    public Airbus(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void printModel() {
+        System.out.println("Модель самолета: " + name);
+    }
+
+    public void printCountEngine() {
+        System.out.println("Количество двигателей равно: " + COUNT_ENGINE);
+    }
+
+    @Override
+    public String toString() {
+        return "Airbus{"
+                + "name='" + name + '\''
+                + '}';
+    }
+}
