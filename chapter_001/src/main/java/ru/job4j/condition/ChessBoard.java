cad9254f8075743deffd747b7e856308ffc87c2b
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2020-10-05 21:26:28 +0300

    Math.abs [#373616]

---
+++ chapter_001/src/main/java/ru/job4j/condition/ChessBoard.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class ChessBoard {
+    public static int way(int x1, int y1, int x2, int y2) {
+        int rsl = 0;
+        if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
+            rsl = Math.abs(x2 - x1);
+        }
+        return rsl;
+    }
+}
