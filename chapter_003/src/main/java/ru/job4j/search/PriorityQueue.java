960a2e0883f8cea5265195f754f366c402cc04ae
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2020-12-20 21:15:22 +0300

    PriorityQueue. Add JavaDoc[#259165]

--- chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
+++ chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -2,13 +2,23 @@ package ru.job4j.search;
 
 import java.util.LinkedList;
 
+/**
+ * Класс описывает работу простейшей очереди по приоритету, которая работает
+ * по принципу FIFO (first in - first out)
+ * @author STAS KOROBEYNIKOV
+ * @version 1.0
+ */
 public class PriorityQueue {
-
+    /**
+     * Хранение задание осуществляется в коллекции типа LinkedList
+     */
     private LinkedList<Task> tasks = new LinkedList<>();
 
     /**
-     * Метод должен вставлять в нужную позицию элемент.
-     * @param task задача
+     * Метод принимает на вход заявку на вход и добавляет ее в очередь.
+     * Если встречаются 2 задания с одинаковым приоритетом, то в очереди
+     * они распределяются по принципу FIFO.
+     * @param task задача которая добавляется в очередь
      */
     public void put(Task task) {
         var i = 0;
@@ -21,6 +31,10 @@ public class PriorityQueue {
         tasks.add(i, task);
     }
 
+    /**
+     * Метод позволяет получить первую задачу в очереди
+     * @return возвращает задачу из головы очереди или null если очередь пуста
+     */
     public Task take() {
         return this.tasks.poll();
     }
