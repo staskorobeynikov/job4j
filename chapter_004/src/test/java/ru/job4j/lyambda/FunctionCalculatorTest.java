ef5f85f7fca29ef9f621e8510a26954572e61575
staskorobeynikov <stas.korobeinikov@mail.ru>
2021-11-27 16:51:00 +0300

    edit name tests and test with exponential function

--- chapter_004/src/test/java/ru/job4j/lyambda/FunctionCalculatorTest.java
+++ chapter_004/src/test/java/ru/job4j/lyambda/FunctionCalculatorTest.java
@@ -14,25 +14,24 @@ public class FunctionCalculatorTest {
     @Test
     public void whenLinearFunctionThenLinearResults() {
         FunctionCalculator function = new FunctionCalculator();
-        List<Double> result = function.diapason(5, 8, (x) -> 2 * x + 1);
+        List<Double> result = function.diapason(5, 8, x -> 2 * x + 1);
         List<Double> expected = Arrays.asList(11D, 13D, 15D, 17D);
         assertThat(result, is(expected));
     }
 
     @Test
-    public void whenLinearFunctionThenQuadratDataResults() {
+    public void whenQuadraticFunctionThenQuadraticDataResults() {
         FunctionCalculator function = new FunctionCalculator();
-        List<Double> result = function.diapason(1, 5, (x) -> x * x + 5);
+        List<Double> result = function.diapason(1, 5, x -> x * x + 5);
         List<Double> expected = Arrays.asList(6D, 9D, 14D, 21D, 30D);
         assertThat(result, is(expected));
     }
 
     @Test
-    public void whenLinearFunctionThenLogarithmDataResults() {
+    public void whenExponentialFunctionThenExponentialDataResults() {
         FunctionCalculator function = new FunctionCalculator();
-        List<Double> result = function.diapason(1, 3, (x) -> 5 * Math.log(x) + 5);
-        assertEquals(result.get(0), 5D, 0.01);
-        assertEquals(result.get(1), 8.46D, 0.01);
-        assertEquals(result.get(2), 10.49D, 0.01);
+        List<Double> result = function.diapason(1, 3, x -> Math.pow(2, x));
+        List<Double> expected = Arrays.asList(2D, 4D, 8D);
+        assertThat(result, is(expected));
     }
 }
